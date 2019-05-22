b8b699680cb1c71961c7e53dfd84d7ad68c8fe72
Alex Au <12609703@student.uts.edu.au>
2019-05-22 11:58:35 +1000

    Added Pokemon font to first 2 screens
    Adjusted animation from 0.25 to 0.5

--- groupass/groupass/SegueToRight.swift
+++ groupass/groupass/SegueToRight.swift
@@ -15,7 +15,7 @@ class SegueFromRight: UIStoryboardSegue {
         
         src.view.superview?.insertSubview(dst.view, aboveSubview: src.view)
         dst.view.transform = CGAffineTransform(translationX: src.view.frame.size.width*2, y: 0) //Double the X-Axis
-        UIView.animate(withDuration: 0.25, delay: 0.0, options: UIView.AnimationOptions.curveEaseInOut, animations: {
+        UIView.animate(withDuration: 0.5, delay: 0.0, options: UIView.AnimationOptions.curveEaseInOut, animations: {
             dst.view.transform = CGAffineTransform(translationX: 0, y: 0)
         }) { (finished) in
             src.present(dst, animated: false, completion: nil)
